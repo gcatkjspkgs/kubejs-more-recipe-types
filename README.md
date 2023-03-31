0d9944767e2a6586c7d87a1404e8089337ebdf2b
Gcat101 <gcatmail2@gmail.com>
2023-03-31 17:46:13 +0300

    Industrial foregoing recipes

--- README.md
+++ README.md
@@ -6,8 +6,24 @@ All new types are functions inside of the `global` object. They should be called
 
 ## Types
 
-### Powah energizing
+### Powah
+
+#### Energizing
 
 ```js
-global.more_recipe_types.powah.energizing(event, [<Input items>], <Output item>)
+global.more_recipe_types.powah.energizing(event, [<Input items>], <Output item>, <Energy (100 by default)>)
+```
+
+### Industrial Foregoing
+
+#### Dissolution Chamber
+
+```js
+global.more_recipe_types.industrialforegoing.dissolution_chamber(event, [<Input items>], Fluid.of(<Input fluid>, <Amount (1b by default)>), <Output item>, <Output fluid (nothing by default)>, <Time in ticks (20 by default)>)
+```
+
+#### Fluid Extractor
+
+```js
+global.more_recipe_types.industrialforegoing.fluid_extractor(event, <Input block>, Fluid.of(<Output fluid>, <Amount (1b by default)>), <Block damage chance (float, 0% by default)>, <Result block ("minecraft:air" by default)>)
 ```
--- startup_scripts/moretypes.js
+++ startup_scripts/moretypes.js
@@ -1,20 +1,68 @@
+function arrConvert(i) {
+	if (!Array.isArray(i)) i = [i]
+	return i
+}
+function ingredientsConvert(i) {
+	let ingredients = []
+	i.forEach(item => {ingredients.push(Ingredient.of(item))})
+	return ingredients
+}
+function fluidConvert(i) {
+	if (typeof i == "string") i = Fluid.of(i, 1000)
+	return i
+}
+
 onEvent("loaded", e => {
 	global.more_recipe_types = {
 		powah: {
 			energizing: (event, input, output, energy) => {
+				input = arrConvert(input).slice(0, 6)
 				if (energy==null) energy=100
-				if (!Array.isArray(input)) input = [input]
-				
-				let ingredients = []
-				input.forEach(i => {ingredients.push(Ingredient.of(i))})
 
 				event.custom({
 					type: "powah:energizing",
 
-					ingredients: ingredients,
+					ingredients: ingredientsConvert(input),
 					result: Ingredient.of(output),
 
 					energy: energy
+				})
+			}
+		},
+
+		industrialforegoing: {
+			dissolution_chamber: (event, input, inputFluid, output, outputFluid, time) => {
+				input = arrConvert(input).slice(0, 6)
+				inputFluid = fluidConvert(inputFluid)
+				outputFluid = fluidConvert(outputFluid)
+				if (time==null) time = 20
+
+				event.custom({
+					type: "industrialforegoing:dissolution_chamber",
+					
+					input: ingredientsConvert(input),
+					inputFluid: `{FluidName:"${inputFluid.id}",Amount:${inputFluid.amount}}`,
+					
+					processingTime: time,
+		
+					output: Ingredient.of(output),
+					outputFluid: `{FluidName:"${outputFluid!=undefined ? outputFluid.id : ""}",Amount:${outputFluid!=undefined ? outputFluid.amount : 0}}`
+				})
+			},
+			fluid_extractor: (event, input, output, breakchance, result) => {
+				output = fluidConvert(output)
+				if (breakchance==null) breakchance = 0
+				if (result==null) result = "minecraft:air"
+
+				event.custom({
+					type: "industrialforegoing:fluid_extractor",
+		
+					input: Ingredient.of(input),
+					result: result,
+					output: `{FluidName:"${output.id}",Amount:${output.amount}}`,
+					
+					breakChance: breakchance,
+					defaultRecipe: false
 				})
 			}
 		}
